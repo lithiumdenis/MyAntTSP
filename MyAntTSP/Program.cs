f24f78a6bcf989086f54677679b76b8ae64e806e
Denis <lithiumdenis@gmail.com>
2017-04-05 15:37:42 +0300

    Matrix of distations generation

--- MyAntTSP/Program.cs
+++ MyAntTSP/Program.cs
@@ -8,6 +8,23 @@ namespace MyAntTSP
 {
     class Program
     {
+        /// <summary>
+        /// Печать квадратной матрицы в консоль
+        /// </summary>
+        /// <param name="D">Квадратная матрица</param>
+        private static void printMatrix(double[,] D)
+        {
+            for (int i = 0; i < D.GetLength(0); i++)
+            {
+                for (int j = 0; j < D.GetLength(1); j++)
+                {
+                    Console.Write("(" + i + "," + j + "): " + D[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             //var N_MIN = 3;      // минимальное количество вершин
@@ -21,10 +38,42 @@ namespace MyAntTSP
 
 
             //Количество вершин
-            var N = 10;
+            var N = 5;
             //Инициализация матрицы расстояний
-            double[,] D = new double[N, N]; ;
+            double[,] D = new double[N, N];
 
+            //Создание нового генератора ПСП
+            Random rnd = new Random();
+
+            //Заполнение матрицы расстояний случайными значениями
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    //Расстояние из i в i равно 0, как изначально и есть
+                    if (i != j)
+                    {
+                        //Если симметричное расстояние еще не заполнено
+                        //Расстояние из i в j эквивалентно расстоянию из j в i 
+                        if (D[j, i] == 0)
+                        {
+                            //Присваиваем случайное целочисленное расстояние из промежутка (вершины не совпадают, так что не с 0)
+                            D[i, j] = rnd.Next(1, 1000);
+
+                        }
+                        else
+                        {
+                            //Присваиваем симметричное значение
+                            D[i, j] = D[j, i];
+                        }
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine("Матрица расстояний:");
+            printMatrix(D);
+            
 
             int A = 0, B = 0;
 
