43679e0a0d4f1c5eef664f5434baf1d3594d738b
Denis <lithiumdenis@gmail.com>
2017-04-05 14:51:19 +0300

    First commit

---
+++ MyAntTSP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAntTSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var N_MIN = 3;      // минимальное количество вершин
+            //var N_MAX = 30;     // максимальное количество вершин
+            var ALPHA = 1;      // вес фермента
+            var BETA = 3;       // коэффициент эвристики
+            var T_MAX = 100;    // время жизни колонии
+            var M = 20;         // количество муравьев в колонии
+            var Q = 100;        // количество феромона
+            var RHO = 0.5;      // коэффициент испарения феромона
+
+
+            //Количество вершин
+            var N = 10;
+            //Инициализация матрицы расстояний
+            double[,] D = new double[N, N]; ;
+
+
+            int A = 0, B = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
